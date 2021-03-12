e014e6a7aba716c18bb75d06585ecfb14de7caac
Ivan Sales <ivan.sales@enti.cat>
2021-03-12 10:57:45 +0100

    Camara sigue al jugador. Creacion script CameraController

---
+++ Assets/Scripts/CameraController.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour
+{
+
+    public Camera cam;
+    public float zoom;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        //cam = Camera.main;
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        cam.fieldOfView = Mathf.Lerp(cam.fieldOfView, zoom, Time.deltaTime);
+    }
+}
