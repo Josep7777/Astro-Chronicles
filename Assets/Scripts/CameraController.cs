2b13612d1c3050f4aac5c47f4e812a4e3a1c25b3
Ivan Sales <ivan.sales@enti.cat>
2021-03-19 10:16:52 +0100

    Tutorial del juego aun no terminado

--- Assets/Scripts/CameraController.cs
+++ Assets/Scripts/CameraController.cs
@@ -5,19 +5,54 @@ using UnityEngine;
 public class CameraController : MonoBehaviour
 {
 
-    public Camera cam;
+    private Camera camara;
     public float zoom;
-
+    private bool flag_tutorial;
+    private GameObject jugador;
     // Start is called before the first frame update
     void Start()
     {
-        //cam = Camera.main;
+        
+        camara = Camera.main;
+        flag_tutorial = true;
+        jugador = GameObject.FindGameObjectWithTag("Player");
 
     }
 
     // Update is called once per frame
     void Update()
     {
-        cam.fieldOfView = Mathf.Lerp(cam.fieldOfView, zoom, Time.deltaTime);
+        if (flag_tutorial) tutorial();
+        else seguirJugador();
+
+        //camara.fieldOfView = Mathf.Lerp(camara.fieldOfView, zoom, Time.deltaTime);
+        
+    }
+
+    void tutorial()
+    {
+        camara.orthographicSize = zoom; //Ajustar el zoom
+        camara.transform.position = new Vector3(0, 0, -10);
+        
+        if(jugador.transform.position.x < camara.transform.position.x - zoom-2)
+        {
+            jugador.transform.position = new Vector2(camara.transform.position.x - zoom - 2, jugador.transform.position.y);
+        }
+
+        if (jugador.transform.position.x > camara.transform.position.x + zoom+2)
+        {
+            jugador.transform.position = new Vector2(camara.transform.position.x + zoom + 2, jugador.transform.position.y);
+        }
+
+
+    }
+
+    void seguirJugador()
+    {
+
+        if (jugador != null)
+        {
+            camara.transform.position = new Vector3(jugador.transform.position.x, jugador.transform.position.y, -10);
+        }
     }
 }
