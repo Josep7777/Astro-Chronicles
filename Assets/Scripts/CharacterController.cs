ac935c560127bf617b33be4e32ec169320992235
Ivan Sales <ivan.sales@enti.cat>
2021-03-19 11:04:39 +0100

    Faltan arreglar los saltos con el nivel

--- Assets/Scripts/CharacterController.cs
+++ Assets/Scripts/CharacterController.cs
@@ -10,9 +10,18 @@ public class CharacterController : MonoBehaviour
     public bool estaensuelo = false;
     public bool rebotar = false;
     public bool rebotari = false;
+
+    private bool flag_salto;
+    private int flag_rebote_izqui;
+    private int flag_rebote_dere;
+    private bool flag_tiempo;
+    private float tiempo_aux;
     void Start()
     {
-        
+        flag_salto = false;
+        flag_rebote_dere = 0;
+        flag_rebote_izqui = 0;
+        flag_tiempo = false;
     }
 
     // Update is called once per frame
@@ -38,20 +47,41 @@ public class CharacterController : MonoBehaviour
         Saltar();
         Rebotar();
         Rebotari();
-       
-       
-       
-       
+
         Vector3 movement = new Vector3(Input.GetAxis("Horizontal"), 0f, 0f);
         transform.position += movement * Time.deltaTime * velocidad;
+
+        if (flag_rebote_dere == 2 || flag_rebote_izqui == 2)
+        {
+            if (flag_tiempo == false) { 
+                tiempo_aux = Time.deltaTime;
+                flag_tiempo = true;
+            }
+            
+            if (Time.deltaTime > tiempo_aux + 0.4f) {
+                flag_rebote_dere = 0;
+                flag_rebote_izqui = 0;
+            }
+        } else
+        {
+            flag_tiempo = false;
+        }
     }
 
     void Saltar()
     {
         if ((Input.GetKey(KeyCode.Space) || Input.GetKey(KeyCode.W)) &&  estaensuelo == true) 
         {
-            gameObject.GetComponent<Rigidbody2D>().AddForce(new Vector2(0f, 1.5f), ForceMode2D.Impulse);
-
+            if (flag_salto == false)
+            {
+                gameObject.GetComponent<Rigidbody2D>().AddForce(new Vector2(0f, 7.0f), ForceMode2D.Impulse);
+                flag_salto = true;
+                Debug.Log("Salto");
+            }
+            //Debug.Log("Entra");
+        } else
+        {
+            flag_salto = false;
         }
 
     }
@@ -59,10 +89,18 @@ public class CharacterController : MonoBehaviour
     void Rebotar()
     {
 
-        if (Input.GetKey(KeyCode.Space) && rebotar == true)
+        if ((Input.GetKey(KeyCode.Space) || Input.GetKey(KeyCode.W)) && rebotar == true)
         {
-            gameObject.GetComponent<Rigidbody2D>().AddForce(new Vector2(-0.25f, 0.25f), ForceMode2D.Impulse);
+            if (flag_rebote_dere==0)
+            {
+                gameObject.GetComponent<Rigidbody2D>().AddForce(new Vector2(-2.0f, 3.0f), ForceMode2D.Impulse);
+                flag_rebote_dere = 1;
+            }
+            
             //rebotari= true;
+        } else
+        {
+            flag_rebote_dere = 2;
         }
 
        
@@ -72,9 +110,18 @@ public class CharacterController : MonoBehaviour
     void Rebotari()
     {
 
-        if (Input.GetKey(KeyCode.Space) && rebotari == true)
+        if ((Input.GetKey(KeyCode.Space) || Input.GetKey(KeyCode.W)) && rebotari == true)
         {
-            gameObject.GetComponent<Rigidbody2D>().AddForce(new Vector2(0.25f, 0.25f), ForceMode2D.Impulse);
+            if (flag_rebote_izqui==0)
+            {
+                gameObject.GetComponent<Rigidbody2D>().AddForce(new Vector2(2.0f, 3.0f), ForceMode2D.Impulse);
+                flag_rebote_izqui = 1;
+                Debug.Log("Izqui");
+            }
+            
+        } else
+        {
+            flag_rebote_izqui = 2;
         }
     }
 }
