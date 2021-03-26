4f1832762910c31f45614d27ca96a332f7872cdc
Pablo Perpiñan <pablo.perpinan@enti.cat>
2021-03-26 11:22:44 +0100

    cambio enemigo volador

--- Assets/Scripts/patron.cs
+++ Assets/Scripts/patron.cs
@@ -7,6 +7,7 @@ public class patron : MonoBehaviour
 
     public float velocidad;
     public float area;
+    public float area2;
     public float areadisparo;
     public float velocidadisparo;
     public float fireRate = 1f;
@@ -14,16 +15,20 @@ public class patron : MonoBehaviour
     public GameObject bullet;
     public GameObject bulletParent;
     private Transform player;
-   // private bool flagb = false;
+    private float posicioninicial;
+    // private bool flagb = false;
     private Transform A;
     private Transform B;
+    private bool flagAB = false;
+    private Transform enemy;
 
     void Start()
     {
-
+        enemy = GameObject.FindGameObjectWithTag("EnemigoA").transform;
         player = GameObject.FindGameObjectWithTag("Player").transform;
         A = GameObject.FindGameObjectWithTag("A").transform;
         B = GameObject.FindGameObjectWithTag("B").transform;
+        posicioninicial = A.position.x;
     }
 
 
@@ -31,8 +36,9 @@ public class patron : MonoBehaviour
     {
 
         float distancia = Vector2.Distance(player.position, transform.position);
-
-        if(distancia< area && distancia>areadisparo)
+        float distancia2 = Vector2.Distance(A.position, transform.position);
+        float distancia3 = Vector2.Distance(B.position, transform.position);
+        if (distancia< area && distancia>areadisparo)
         {
             transform.position = Vector2.MoveTowards(this.transform.position, player.position, velocidad * Time.deltaTime);
         }
@@ -43,28 +49,28 @@ public class patron : MonoBehaviour
 
            transform.position = Vector2.MoveTowards(this.transform.position, A.position, velocidad * Time.deltaTime);
 
-            //if(this.transform.position == A.position)
-            // {
-            //flagb = true;
+            if (distancia2 < area2)
+            {
+                //Debug.Log("aaaaaa1");
+                if (flagAB == false)
+                {
+                    Debug.Log("Hola");
+                    transform.position = Vector2.MoveTowards(enemy.position, B.position, velocidad * Time.deltaTime);
+                    //  Debug.Log("a: " + this.transform.position.x);
+                    // Debug.Log(B.position.x);
+                }
 
-            //     transform.position = Vector2.MoveTowards(this.transform.position, B.position, velocidad * Time.deltaTime);
 
-
-            // }
-          
-
-           // if (flagb)
-           // {
-           //     transform.position = Vector2.MoveTowards(this.transform.position, B.position, velocidad * Time.deltaTime);
-       
-           // }
-
-           // if (this.transform.position == B.position)
-           // {
-            //    flagb = false;
-//
-
-           // }
+                if (distancia3 < area2)
+               {
+                    Debug.Log("adios");
+                    
+                    //this.transform.position = new Vector2(posicioninicial, this.transform.position.y);
+                    transform.position = Vector2.MoveTowards(this.transform.position, A.position, velocidad * Time.deltaTime);
+                    if (this.transform.position == A.position) flagAB = false;
+                    else flagAB = true;
+                }
+            }
 
 
 
@@ -83,6 +89,7 @@ public class patron : MonoBehaviour
     private void OnDrawGizmosSelected()
     {
         Gizmos.color = Color.white;
+        Gizmos.DrawWireSphere(transform.position, area2);
         Gizmos.DrawWireSphere(transform.position, area);
         Gizmos.DrawWireSphere(transform.position, areadisparo);
     }
