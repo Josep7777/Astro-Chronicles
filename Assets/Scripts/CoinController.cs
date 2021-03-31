c7176c9c75f4537ad9dea8a1170afa9a4c7cae2e
Ivan Sales <ivan.sales@enti.cat>
2021-03-31 19:59:44 +0200

    Monedas done

---
+++ Assets/Scripts/CoinController.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.UI;
+using UnityEngine;
+
+public class CoinController : MonoBehaviour
+{
+    private float monedas;
+    private bool aux;
+    
+    public Text contador;
+
+    private void Start()
+    {
+        monedas = 0;
+        aux = false;
+    }
+
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        //Debug.Log("Ey");
+        if (other.gameObject.tag == "Moneda")
+        {
+            Destroy(other.gameObject);
+            //if (aux == false)
+            //{
+            monedas++;
+                //aux = true;
+            //}
+            //Debug.Log("Ey");
+            contador.text = monedas.ToString();
+            
+        }  
+    }
+}
