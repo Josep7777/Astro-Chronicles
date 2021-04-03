1ff9ea508a5c38c1c7c70d7c48154b4f8726d5bf
Josep <josep.romera@enti.cat>
2021-04-03 18:08:20 +0200

    EnemyMelee

---
+++ Assets/Scripts/Character/PlayerHealth.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerHealth : MonoBehaviour
+{
+    public float playerMaxHealth;
+    private float playerHealth;
+
+
+
+    void Start()
+    {
+        playerHealth = playerMaxHealth;
+
+    }
+
+    private void Update()
+    {
+        Debug.Log(playerHealth);
+
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.tag.Equals("enemy"))
+        {
+            playerHealth--;
+        }
+    }
+}
