b4ce7c068f05a085860c8336a82071be3e4a177d
Pablo Perpiñan <pablo.perpinan@enti.cat>
2021-04-05 20:00:16 +0200

    enemigos acabados? yo digo si

--- Assets/Scripts/Enemies/EnemyPatrol.cs
+++ Assets/Scripts/Enemies/EnemyPatrol.cs
@@ -5,14 +5,23 @@ using UnityEngine;
 public class EnemyPatrol : MonoBehaviour
 {
  public float speed;
+    public float area;
     public float distance;
-
+    private Transform player;
     private bool movingRight = true;
 
     public Transform groundDetection;
 
+
+    private void Start()
+    {
+        player = GameObject.FindGameObjectWithTag("Player").transform;
+
+    }
+
     private void Update()
     {
+        float distancia = Vector2.Distance(player.position, transform.position);
         transform.Translate(Vector2.right * speed * Time.deltaTime);
 
         RaycastHit2D groundInfo = Physics2D.Raycast(groundDetection.position, Vector2.down, distance);
@@ -28,5 +37,20 @@ public class EnemyPatrol : MonoBehaviour
                 movingRight = true;
             }
         }
+
+        if (distancia < area)
+        {
+            transform.position = Vector2.MoveTowards(this.transform.position, player.position, speed * Time.deltaTime);
+
+
+        }
+    }
+
+    private void OnDrawGizmosSelected()
+    {
+        Gizmos.color = Color.white;
+       
+        Gizmos.DrawWireSphere(transform.position, area);
+       
     }
 }
