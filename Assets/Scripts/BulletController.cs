af81d0bc2bb7243e1046e77c6b924d7406874fd4
Ivan Sales <ivan.sales@enti.cat>
2021-04-05 19:09:39 +0200

    Bala se destruye con pared, tiene particula y bug solucionado

---
+++ Assets/Scripts/BulletController.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BulletController : MonoBehaviour
+{
+    public GameObject ps;
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        Instantiate(ps, transform.position, transform.rotation);
+        Destroy(gameObject);
+    }
+}
