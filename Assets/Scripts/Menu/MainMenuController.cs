ae05e5a2ac19493cceb9d27829a32d99757763e8
Ivan Sales <ivan.sales@enti.cat>
2021-04-23 01:00:04 +0200

    Menú casi acabado, falta volumen porque no hay música

--- Assets/Scripts/Menu/MainMenuController.cs
+++ Assets/Scripts/Menu/MainMenuController.cs
@@ -2,21 +2,57 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class MainMenuController : MonoBehaviour
 {
+
+    Resolution[] resoluciones_disponibles;
+    public Dropdown dropdown;
+    private List<string> resoluciones = new List<string>();
+    private int index;
+
+    private void Start()
+    {
+        resoluciones_disponibles = Screen.resolutions;
+
+        dropdown.ClearOptions();
+
+        for(int i=0;i< resoluciones_disponibles.Length; i++)
+        {
+            string resolucion = resoluciones_disponibles[i].width + "x" + resoluciones_disponibles[i].height;
+            resoluciones.Add(resolucion);
+
+            if (resoluciones_disponibles[i].width == Screen.currentResolution.width && resoluciones_disponibles[i].height == Screen.currentResolution.height)
+            {
+                index = i;
+            }
+        }
+
+        dropdown.AddOptions(resoluciones);
+        dropdown.value = index;
+        dropdown.RefreshShownValue();
+    }
+
+
+    public void Resoluciones(int i)
+    {
+        Resolution r = resoluciones_disponibles[i];
+        Screen.SetResolution(r.width, r.height, Screen.fullScreen);
+    }
+
     public void NuevoJuego()
     {
         SceneManager.LoadScene("Nivel1");
     }
 
-    public void Opciones()
+    public void FullScreen(bool fs)
     {
-
+        Screen.fullScreen = fs;
     }
 
     public void Salir()
     {
-
+        Application.Quit();
     }
 }
