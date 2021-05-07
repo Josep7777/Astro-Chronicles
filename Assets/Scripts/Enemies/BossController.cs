67ba0460b376a1c1f6cb0cda9a78e79643efd876
Ivan Sales <ivan.sales@enti.cat>
2021-05-07 10:32:34 +0200

    Añadidas mejoras

--- Assets/Scripts/Enemies/BossController.cs
+++ Assets/Scripts/Enemies/BossController.cs
@@ -4,15 +4,19 @@ using UnityEngine;
 
 public class BossController : MonoBehaviour
 {
-
     private int ataque;
     public float cd_ataques = 0;
-    public bool ataque1 = false;
+    public bool ataque_acabado = true;
     private GameObject jugador;
+    public GameObject cadena;
+    private ChainController cc;
+    private float contador=0;
+    private bool ataque2=false;
     // Start is called before the first frame update
     void Start()
     {
         jugador = GameObject.FindGameObjectWithTag("Player");
+        cc = cadena.gameObject.GetComponent<ChainController>();
     }
 
     // Update is called once per frame
@@ -20,40 +24,53 @@ public class BossController : MonoBehaviour
     {
         if (cd_ataques <= 0)
         {
-            ataque = Random.Range(0, 3);
+            if(ataque2==false) ataque = Random.Range(0, 3);
 
             switch (ataque)
             {
                 case 0:
-                    ataque1 = true;
-                    cd_ataques = 2f;
+                    cd_ataques = Random.Range(1,5);
+                    ataque_acabado = false;
+                    cc.num_ataque1 = 3;
                     Debug.Log("A");
                     break;
 
                 case 1:
+                    //cd_ataques = Random.Range(2, 5);
+                    //contador = 0.5f;
+                    ataque2 = true;
+                    ataque_acabado = false;
                     Embestida();
-                    cd_ataques = 2f;
                     Debug.Log("B");
                     break;
 
                 case 2:
                     //Embestida();
-                    cd_ataques = 2f;
+                    cd_ataques = Random.Range(1, 5);
                     Debug.Log("C");
                     break;
             }
         } else
         {
-            cd_ataques -= Time.deltaTime;
+            if(ataque_acabado) cd_ataques -= Time.deltaTime;
         }
-        //Embestida();
+
+        //this.gameObject.GetComponent<Rigidbody2D>().AddForce(new Vector2(jugador.transform.position.x*2, 0), ForceMode2D.Impulse);
     }
 
     public void Embestida()
     {
-        
-        //Debug.Log(GameObject.FindGameObjectWithTag("Player").transform.position);
-        this.gameObject.GetComponent<Rigidbody2D>().AddForce(new Vector2(jugador.transform.position.x, 0), ForceMode2D.Impulse);
+        Debug.Log("EA");
+        this.gameObject.GetComponent<Rigidbody2D>().AddForce(new Vector2(jugador.transform.position.x*2, 0), ForceMode2D.Impulse);
 
+        contador += Time.deltaTime;
+
+        if (contador >= 0.3f)
+        {
+            ataque2 = false;
+            contador = 0;
+            cd_ataques = Random.Range(1, 5);
+            ataque_acabado = true;
+        }
     }
 }
--- Assets/Scripts/Others/ChainController.cs
+++ Assets/Scripts/Others/ChainController.cs
@@ -2,23 +2,36 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class PruebaControlle4r : MonoBehaviour
+public class ChainController : MonoBehaviour
 {
     private float contador = 1;
     private float contador_ataque1 = 0;
     private bool flag_rot_dere = false;
     private bool flag_rot_izqui = false;
     private bool aux = false;
+    public int num_ataque1 = 0;
+    private BossController bc;
+    private bool no_repetir = false;
     // Start is called before the first frame update
     void Start()
     {
-
+        num_ataque1 = 0;
+        bc = GameObject.FindGameObjectWithTag("Boss").GetComponent<BossController>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        //Ataque1();
+        if (num_ataque1 >= 0)
+        {
+            Ataque1();
+            no_repetir = true;
+        }
+        else if(no_repetir)
+        {
+            bc.ataque_acabado = true;
+            no_repetir = false;
+        }
         //Ataque2();
     }
 
@@ -31,6 +44,7 @@ public class PruebaControlle4r : MonoBehaviour
                 //flag_rot_dere = false;
                 flag_rot_izqui = true;
                 contador = 0;
+                num_ataque1 -= 1;
             } else
             {
                 //flag_rot_dere = false;
@@ -39,7 +53,6 @@ public class PruebaControlle4r : MonoBehaviour
             }
 
             //final_lvl1 = true;
-            Debug.Log("Hola");
             //SceneManager.LoadScene("FinalLvl1");
         }
     }
@@ -48,14 +61,18 @@ public class PruebaControlle4r : MonoBehaviour
     {
         if (contador <= 0.5 && flag_rot_izqui)
         {
-            this.gameObject.GetComponent<Rigidbody2D>().AddForce(new Vector2(-1f, 5), ForceMode2D.Impulse);
+            this.gameObject.GetComponent<Rigidbody2D>().AddForce(new Vector2(-2f, 5), ForceMode2D.Impulse);
             contador += Time.deltaTime;
+            
+
+            //Debug.Log("Dere");
             //contador -= Time.deltaTime;
         }
         else if (contador <= 0.5 && flag_rot_izqui == false)
         {
-            this.gameObject.GetComponent<Rigidbody2D>().AddForce(new Vector2(1f, 5), ForceMode2D.Impulse);
+            this.gameObject.GetComponent<Rigidbody2D>().AddForce(new Vector2(2f, 5), ForceMode2D.Impulse);
             contador += Time.deltaTime;
+            
         }
     }
 }
