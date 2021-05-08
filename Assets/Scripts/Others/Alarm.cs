56c79d73dd11ad85bb86c0255b5d0d5b002e1037
Josep <josep.romera@enti.cat>
2021-05-08 06:46:10 +0200

    Nivel 2 +

---
+++ Assets/Scripts/Others/Alarm.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Alarm : MonoBehaviour
+{
+    public AudioSource alarmSound;
+    private Vector3 posA;
+    private Vector3 posB;
+    private Vector3 nextPos;
+
+
+    public float speed;
+
+    public Transform childTransform;
+
+    public Transform transformB;
+    // Start is called before the first frame update
+    void Start()
+    {
+        posB = childTransform.localPosition;
+        posB = transformB.localPosition;
+        nextPos = posB;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Move();
+    }
+
+    private void Move()
+    {
+        childTransform.localPosition = Vector3.MoveTowards(childTransform.localPosition, nextPos, speed * Time.deltaTime);
+
+    }
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.tag == "Player")
+        {
+            //alarmSound.Play();
+        }
+    }
+}
