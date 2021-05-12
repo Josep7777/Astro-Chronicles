dfb350af08d2c2aef63481950b7637c3c0815618
Ivan Sales <ivan.sales@enti.cat>
2021-05-12 22:56:45 +0200

    Los 3 ataques principales acabados

--- Assets/Scripts/Enemies/BossController.cs
+++ Assets/Scripts/Enemies/BossController.cs
@@ -6,7 +6,9 @@ public class BossController : MonoBehaviour
 {
     private int ataque;
     public float cd_ataques = 0;
-    public bool ataque_acabado = true;
+    public bool ataque_acabado1 = true;
+    public bool ataque_acabado2 = true;
+    public bool ataque_acabado3 = true;
     private GameObject jugador;
     public GameObject cadena;
     public GameObject cadena_entera;
@@ -26,35 +28,36 @@ public class BossController : MonoBehaviour
     {
         if (cd_ataques <= 0)
         {
-            if (ataque2 == false) ataque = 2; //ataque = Random.Range(0, 3);
+            if (ataque2 == false) ataque = Random.Range(0, 3); //ataque = 1; //ataque = Random.Range(0, 3); //ataque = 2; //
 
             switch (ataque)
             {
                 case 0:
                     cd_ataques = Random.Range(1,5);
-                    ataque_acabado = false;
+                    ataque_acabado1 = false;
                     cc.num_ataque1 = 3;
                     Debug.Log("A");
                     break;
 
                 case 1:
+                    Debug.Log("B");
                     //cd_ataques = Random.Range(1, 5);
                     ataque2 = true;
-                    ataque_acabado = false;
+                    ataque_acabado2 = false;
                     Embestida();
-                    Debug.Log("B");
                     break;
 
                 case 2:
                     //Embestida();
+                    ataque_acabado3 = false;
                     cc.ataque3 = true;
-                    //cd_ataques = Random.Range(1, 5);
-                    //Debug.Log("C");
+                    cd_ataques = Random.Range(1, 5);
+                    Debug.Log("C");
                     break;
             }
         } else
         {
-            if(ataque_acabado) cd_ataques -= Time.deltaTime;
+            if(ataque_acabado1 && ataque_acabado2 && ataque_acabado3) cd_ataques -= Time.deltaTime;
         }
 
         //this.gameObject.GetComponent<Rigidbody2D>().AddForce(new Vector2(jugador.transform.position.x*2, 0), ForceMode2D.Impulse);
@@ -67,17 +70,18 @@ public class BossController : MonoBehaviour
         //this.gameObject.GetComponent<Rigidbody2D>().AddForce(new Vector2(jugador.transform.position.x*2f, 0), ForceMode2D.Impulse);
         this.transform.position = Vector2.MoveTowards(transform.position, new Vector2(jugador.transform.position.x,this.transform.position.y), 5*Time.deltaTime);
         //cadena_entera.transform.position = Vector2.MoveTowards(transform.position, jugador.transform.position, 5 * Time.deltaTime);
+
         contador += Time.deltaTime;
 
         if (contador >= 1f)
         {
-            Debug.Log("Wa");
+            //Debug.Log("Wa");
             this.gameObject.GetComponent<Rigidbody2D>().velocity = Vector3.zero;
             //this.gameObject.GetComponent<Rigidbody2D>().angularVelocity = 0f;
             ataque2 = false;
             contador = 0;
             cd_ataques = Random.Range(1, 5);
-            ataque_acabado = true;
+            ataque_acabado2 = true;
             this.gameObject.GetComponent<Rigidbody2D>().bodyType = RigidbodyType2D.Kinematic;
         }
     }
--- Assets/Scripts/Others/ChainController.cs
+++ Assets/Scripts/Others/ChainController.cs
@@ -12,14 +12,21 @@ public class ChainController : MonoBehaviour
     public int num_ataque1 = 0;
     private BossController bc;
     private bool no_repetir = false;
+    private bool no_repetir2 = false;
     public bool ataque3 = false;
     private float contador2 = 0;
+    private float contador3 = 0;
     public GameObject onda;
+    private GameObject jugador;
+    private bool dere=false;
+    public GameObject particulas;
+    private bool particula_una = false;
     // Start is called before the first frame update
     void Start()
     {
         num_ataque1 = 0;
         bc = GameObject.FindGameObjectWithTag("Boss").GetComponent<BossController>();
+        jugador = GameObject.FindGameObjectWithTag("Player");
     }
 
     // Update is called once per frame
@@ -32,13 +39,22 @@ public class ChainController : MonoBehaviour
         }
         else if(no_repetir)
         {
-            bc.ataque_acabado = true;
+            bc.ataque_acabado1 = true;
             no_repetir = false;
         }
 
         if (ataque3)
         {
             Ataque3();
+            no_repetir2 = true;
+        }
+        else if (no_repetir2)
+        {
+            contador3 = 0;
+            contador2 = 0;
+            particula_una = false;
+            bc.ataque_acabado3 = true;
+            no_repetir2 = false;
         }
         //Ataque2();
     }
@@ -85,21 +101,55 @@ public class ChainController : MonoBehaviour
     void Ataque3()
     {
         contador2 += Time.deltaTime;
+        
         if (contador2 <= 2.0f)
         {
-            this.transform.localScale = new Vector2(5f + contador2, 5f + contador2);
-        } else if(contador2<=2.3f)
-        {
-            this.gameObject.GetComponent<Rigidbody2D>().AddForce(new Vector2(-5f, 5), ForceMode2D.Impulse);
-            onda.transform.position = new Vector2(this.transform.position.x - 1, -2f);
-
-        } else
-        {
+            if (particula_una == false) //Iniciar las particulas
+            {
+                Instantiate(particulas, this.transform.position, Quaternion.identity);
+                particula_una = true;
+            }
             
+            this.transform.localScale = new Vector2(5f + contador2, 5f + contador2); //Escalar
+        }
+        else if (contador2 <= 2.3f)
+        {
+            if (this.transform.position.x - jugador.transform.position.x > 0) //Hacer golpe a la izquierda
+            {
+                this.gameObject.GetComponent<Rigidbody2D>().AddForce(new Vector2(-4f, 5), ForceMode2D.Impulse);
+                onda.transform.position = new Vector2(this.transform.position.x - 1, -2f);
+                dere = false;
+            }
+            //this.gameObject.GetComponent<Rigidbody2D>().AddForce(new Vector2(-2f, 5), ForceMode2D.Impulse);
+            else //Hacer golpe a la derecha
+            {
+                this.gameObject.GetComponent<Rigidbody2D>().AddForce(new Vector2(0.2f, 0.5f), ForceMode2D.Impulse);
+                //Debug.Log("aaaaaaaaaaaaa");
+                onda.transform.position = new Vector2(this.transform.position.x + 1, -2f);
+                dere = true;
+            }
+
+        }
+        else if (contador2 <= 4f) //Hacer la onda
+        {
             onda.SetActive(true);
             //onda.GetComponent<Rigidbody2D>().AddForce(new Vector2(-10f, 0), ForceMode2D.Impulse);
-            onda.transform.position = Vector2.MoveTowards(onda.transform.position, new Vector2(-50f, -2f), 8 * Time.deltaTime);
-            Debug.Log("HOlaaa");
+            if (dere == false)
+                onda.transform.position = Vector2.MoveTowards(onda.transform.position, new Vector2(-50f, -2f), 8 * Time.deltaTime);
+            else
+                onda.transform.position = Vector2.MoveTowards(onda.transform.position, new Vector2(50f, -2f), 8 * Time.deltaTime);
+            //Debug.Log("HOlaaa");
+        }
+        else //Eliminar la onda y volver a escalar arma
+        {
+            onda.SetActive(false);
+            contador3 += Time.deltaTime;
+            if (contador3 <= 2.0f)
+            {
+                //Debug.Log("wasdasd");
+                this.transform.localScale = new Vector2(7f - contador3, 7f - contador3);
+            }
+            else ataque3 = false; //Cuando acabe de escalar, acaba el ataque
         }
     }
 }
