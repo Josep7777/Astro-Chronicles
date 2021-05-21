0a4f9c85e47ebd4c3aee5ff919786a7d1d94064c
Pablo Perpiñan <pablo.perpinan@enti.cat>
2021-05-21 11:02:14 +0200

    enemigo nuevo v1

---
+++ Assets/Scripts/Enemies/Enemy3Patrol.cs
@@ -0,0 +1,104 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy3Patrol : MonoBehaviour
+{
+    public float speed;
+    public float distance;
+    public float velocidad;
+    private bool movingRight = true;
+    private float speed2;
+    public Transform groundDetection;
+    private Transform player;
+    public float area;
+    //private Transform player;
+    public float areadisparo;
+    private bool flagAB = false;
+    private bool flagBA = false;
+    private Transform A;
+    private Transform B;
+    private float C;
+    private float tiempofire;
+    public GameObject bullet;
+    public GameObject bulletParent;
+    public float fireRate = 1f;
+
+    private void Start()
+    {
+        player = GameObject.FindGameObjectWithTag("Player").transform;
+        speed2 = speed * 3 / 2;
+    }
+
+    private void Update()
+    {
+        float distancia = Vector2.Distance(player.position, transform.position);
+
+        if (distancia < areadisparo)
+        {
+
+                   
+
+                if (tiempofire < Time.time)
+                {
+                    Instantiate(bullet, bulletParent.transform.position, Quaternion.identity);
+                    tiempofire = Time.time + fireRate;
+
+                }
+           
+
+
+        }
+         if (distancia < area)
+        {
+            transform.position = Vector2.MoveTowards(this.transform.position, new Vector2(player.position.x, this.transform.position.y), speed2 * Time.deltaTime);
+        }
+        else
+        {
+            transform.Translate(Vector2.right * speed * Time.deltaTime);
+
+            RaycastHit2D groundInfo = Physics2D.Raycast(groundDetection.position, Vector2.down, distance);
+            if (groundInfo.collider == false)
+            {
+                if (movingRight == true)
+                {
+                    transform.eulerAngles = new Vector3(0, -180, 0);
+                    movingRight = false;
+                }
+                else
+                {
+                    transform.eulerAngles = new Vector3(0, 0, 0);
+                    movingRight = true;
+                }
+            }
+        }
+
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.tag == "pared")
+        {
+            transform.eulerAngles = new Vector3(0, -180, 0);
+            movingRight = false;
+        }
+
+        if (collision.gameObject.tag == "izquierda")
+        {
+            transform.eulerAngles = new Vector3(0, 0, 0);
+            movingRight = true;
+        }
+    }
+
+    private void OnDrawGizmosSelected()
+    {
+        Gizmos.color = Color.white;
+        //Gizmos.DrawWireSphere(transform.position, area2);
+        Gizmos.DrawWireSphere(transform.position, area);
+        //Gizmos.DrawWireSphere(transform.position, areadisparo);
+        Gizmos.color = Color.white;
+        //Gizmos.DrawWireSphere(transform.position, area2);
+        Gizmos.DrawWireSphere(transform.position, areadisparo);
+        //Gizmos.DrawWireSphere(transform.position, areadisparo);
+    }
+}
