d59035aeef0c55f0d3f268614e9cce027dd23957
Ivan Sales <ivan.sales@enti.cat>
2021-05-29 23:31:29 +0200

    Creda escena boss2

---
+++ Assets/Scripts/Enemies/Boss2HPController.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Boss2HPController : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public float EnemyMaxHealth;
+    private float EnemyHealth;
+    public Slider barra_vida;
+    public GameObject gc;
+
+    void Start()
+    {
+        EnemyHealth = EnemyMaxHealth;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (EnemyHealth <= 0)
+        {
+            gc.GetComponent<EndController>().flag = true;
+            Destroy(gameObject);
+        }
+    }
+}
