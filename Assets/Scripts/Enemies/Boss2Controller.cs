2ae5b061e3c47fddca29005ef7a4368010fe4810
Ivan Sales <ivan.sales@enti.cat>
2021-05-30 01:02:54 +0200

    Comienzo de jefe

---
+++ Assets/Scripts/Enemies/Boss2Controller.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Boss2Controller : MonoBehaviour
+{
+    private GameObject jugador;
+    // Start is called before the first frame update
+    void Start()
+    {
+        jugador = GameObject.FindGameObjectWithTag("Player");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (this.transform.position.x - jugador.transform.position.x > 0)
+        {
+            transform.eulerAngles = new Vector3(0, -180, 0);
+        } else transform.eulerAngles = new Vector3(0, 0, 0);
+    }
+}
